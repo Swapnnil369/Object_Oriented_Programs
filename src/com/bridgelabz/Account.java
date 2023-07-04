a3ef197eff16763eb0279fd948cb159ad03693b3
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-04 12:28:20 +0530

    Added :Check remaining amount

---
+++ src/com/bridgelabz/Account.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Account {
+    static Scanner scanner = new Scanner(System.in);
+    private double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public void debit(double balance) {
+        System.out.println("Enter the amount to withdraw");
+        double withdraw = scanner.nextDouble();
+        if (withdraw > balance) {
+            System.out.println("Debit amount exceeded account balance");
+        } else
+            balance = balance - withdraw;
+        System.out.println(" Remaining balance is:" + balance);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter the account balance");
+        Account account = new Account(scanner.nextDouble());
+        account.debit(account.balance);
+    }
+
+}
+
